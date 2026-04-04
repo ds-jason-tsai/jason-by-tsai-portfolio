4c600ef960f5da5b1b727094129b0e5dfb60b6a6
Jason TSAI <jason.tsai.0604@gmail.com>
2026-04-04 16:29:53 +0800

    chore: update sitemap domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
   // Replace with the final Vercel Domain provided after deployment
-  const baseUrl = 'https://jasontsai-portfolio.vercel.app';
+  const baseUrl = 'https://jason-by-tsai-portfolio.vercel.app';
   
   const pages = ['', '/services', '/portfolio', '/experience', '/contact'];
   const langs = ['/zh', '/en', '/ja'];
